5c5060a28da7edf9a00dab7e686f3c060cad3ee7
Brian Li <brian.li96@yahoo.com>
2020-11-08 04:45:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(sentence)
+  newArray = ""
+  parsedSentence = sentence.split(" ")
+  parsedSentence.each do |words|
+    newArray += words.reverse + " "
+  end
+  return newArray.strip
+end
+
+def reverse_each_word(sentence)
+  newArray = ""
+  parsedSentence = sentence.split(" ")
+  temp = parsedSentence.collect do |x| 
+    x.reverse
+  end
+  return temp.join(" ")
+end
