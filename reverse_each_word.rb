8fc3537424043eaf3cc2e31141f15449dd0dfdcd
Brian Li <brian.li96@yahoo.com>
2020-11-08 04:13:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   newArray = []
   sentence.split(" ")
   sentence.each do |words|
-    newArray.push(words.reverse)
+    newArray.push(words.reverse())
   end
   newArray.join(" ")
   return newArray
